0f79eb9fe9ff778a80d025bbe148c943673e3c06
Diego Rojas <Diego-27ro@outlook.com>
2022-05-08 21:45:15 -0500

    api with services

---
+++ Routes/index.js
@@ -0,0 +1,14 @@
+const productsRouter = require('./products');
+const categoriesRouter = require('./categories');
+const usersRouter = require('./users');
+const express = require('express');
+
+function routerApi(app) {
+	const router = express.Router(); //router v1
+	app.use('/api/v1', router);
+	router.use('/products', productsRouter);
+	router.use('/categories', categoriesRouter);
+	router.use('/users', usersRouter);
+}
+
+module.exports = routerApi;
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const routerApi = require('./routes');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('Hola mi server en express');
+});
+
+app.listen(port, () => {
+	console.log('listening on port 3000');
+});
+
+routerApi(app);
